8079d38187a5c3433610c6bb690a76116847f331
Anthony Hansord <tonyhansorddev@gmail.com>
2023-02-02 20:17:42 +1100

    completed lab

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,16 @@
 // Code Keypad Component Here
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+function Keypad() {
+  function handleInput() {
+    console.log('Entering password...');
+  }
+
+  return (
+    <div>
+      <p>Enter Passcode</p>
+      <input type="password" onChange={handleInput} />
+    </div>
+  );
 }
 
 export default Keypad;
